89499ed2a5b100cfa080748b24a6cd9511b5a264
Kosuke Takeda <june.bure@gmail.com>
2016-09-14 23:21:47 +0900

    initial commit

---
+++ IsshoMeshi/IsshoMeshi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IsshoMeshi
+//
+//  Created by 竹田 光佑 on 2016/09/14.
+//  Copyright © 2016年 teame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
