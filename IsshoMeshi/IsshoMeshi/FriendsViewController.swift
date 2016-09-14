38739581f6632303a56dbd4b2d2cf51d80349293
Kosuke Takeda <june.bure@gmail.com>
2016-09-15 00:15:42 +0900

    add friends view controller

---
+++ IsshoMeshi/IsshoMeshi/FriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.swift
+//  IsshoMeshi
+//
+//  Created by 竹田 光佑 on 2016/09/14.
+//  Copyright © 2016年 teame. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+
+}
