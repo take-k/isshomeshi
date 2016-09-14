c45f9f4b4cf0b949c2eb19699cb6f8bcd04db485
Kosuke Takeda <june.bure@gmail.com>
2016-09-15 02:20:23 +0900

    API周りの仮実装

---
+++ IsshoMeshi/IsshoMeshi/Router.swift
@@ -0,0 +1,41 @@
+//
+//  Router.swift
+//  IsshoMeshi
+//
+//  Created by 竹田 光佑 on 2016/09/15.
+//  Copyright © 2016年 teame. All rights reserved.
+//
+import Alamofire
+
+enum Router: URLRequestConvertible {
+    static let baseURLString = "localhost:3000"
+    
+    case USERS
+    case USERS_COUNTER_UPDATE([String:AnyObject]?)
+    case USERS_NEW([String:AnyObject]?)
+    case USERS_UPDATE([String:AnyObject]?)
+    
+    
+    var URLRequest: NSMutableURLRequest {
+        
+        let (method, path, parameters) : (String, String, [String: AnyObject]?) = {
+            
+            switch self {
+            case .USERS:
+                return ("POST", "/users.json", nil)
+            case .USERS_COUNTER_UPDATE(let params):
+                return ("GET", "/users.json", params)
+            case .USERS_NEW(let params):
+                return ("GET", "/users.json", params)
+            case .USERS_UPDATE(let params):
+                return ("GET", "/users.json", params)
+            }
+        }()
+        
+        let URL = NSURL(string: Router.baseURLString)
+        let URLRequest = NSMutableURLRequest(URL: URL!.URLByAppendingPathComponent(path))
+        URLRequest.HTTPMethod = method
+        let encoding = Alamofire.ParameterEncoding.URL
+        return encoding.encode(URLRequest, parameters: parameters).0
+    }
+}
--- IsshoMeshi/IsshoMeshi/ViewController.swift
+++ IsshoMeshi/IsshoMeshi/ViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 import Hakuba
-
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     private lazy var hakuba: Hakuba = Hakuba(tableView: self.tableView)
+    
+    let friendModels:[FriendCellModel]? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         let cellModels = ["タケダ","クボ","ハラダ","コモリ"].map { (title) -> CellModel in
             return FriendCellModel(title: "ほげ", selectionHandler: { cell in
             })
         }
         self.hakuba.reset(Section().reset(cellModels).bump()).bump()
+        retrieveUsers()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func retrieveUsers(){
+        Alamofire.request(Router.USERS).responseJSON { (response) in
+            debugPrint(response)
+            switch response.result {
+            case .Success(let value):
+                let json = JSON(value)
+                let friendModels = json.map({ (key,json) -> FriendCellModel in
+                    return FriendCellModel(title: (json["name"].string ?? "タケダ"), selectionHandler: { cell in
+                    })
+                })
+                self.hakuba.sections[0].reset(friendModels).bump()
+            case .Failure(let error):
+                break
+            }
+        }
     }
 
 
