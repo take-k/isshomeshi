4e673a8c1ca5f2d79ca1aa454d5b4f6cf6e9ebe6
Kosuke Takeda <june.bure@gmail.com>
2016-09-16 12:18:10 +0900

    設定

--- IsshoMeshi/IsshoMeshi/ViewController.swift
+++ IsshoMeshi/IsshoMeshi/ViewController.swift
@@ -32,8 +32,6 @@ class ViewController: UIViewController {
     func checkLogin (){
         let defaults = NSUserDefaults.standardUserDefaults()
         
-        defaults.setInteger(0 , forKey: self.myIdKey)
-        
         let myId = defaults.integerForKey(myIdKey)
         let manager = GroupManager.sharedInstance
         if  myId == 0 || manager.myId == 0{
